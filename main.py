01422147d39fe18d21f1c5e7823206be45061088
Valeriya Kaluzhskikh <kuchieriavienko1998@mail.ru>
2025-03-24 22:18:38 +0300

    Вызваны новые функции в модуле main

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-from src.widget import get_date
-from src.widget import mask_account_card
+from src.widget import get_date, mask_account_card
+from src.processing import filter_by_state, sort_by_date
 
+#Маскировка номера карты или счета
 print(mask_account_card(input("Введите номер счета или тип и номер карты")))
+
+#Преобразование даты
 print(get_date("2024-03-11T02:26:18.671407"))
+
+#Сортировка операций по статусу и времени
+print(filter_by_state, sort_by_date)
