4de974bb00d549373c4d240b80ebd9fdbac84d92
Valeriya Kaluzhskikh <kuchieriavienko1998@mail.ru>
2025-03-24 22:29:29 +0300

    Изменения после запуска линтеров

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from src.widget import get_date, mask_account_card
-from src.processing import filter_by_state, sort_by_date
+from src.processing import filter_by_state
+from src.processing import sort_by_date
+from src.widget import get_date
+from src.widget import mask_account_card
 
-#Маскировка номера карты или счета
+# Маскировка номера карты или счета
 print(mask_account_card(input("Введите номер счета или тип и номер карты")))
 
-#Преобразование даты
+# Преобразование даты
 print(get_date("2024-03-11T02:26:18.671407"))
 
-#Сортировка операций по статусу и времени
+# Сортировка операций по статусу и времени
 print(filter_by_state, sort_by_date)
--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,4 @@
-def filter_by_state(operations: list, state: str="EXECUTED") -> list:
+def filter_by_state(operations: list, state: str = "EXECUTED") -> list:
     """Функция фильтрует операции в списке по ключу 'state'"""
     list_of_operation = []
     for item in operations:
@@ -11,4 +11,3 @@ def sort_by_date(operations: list, flow: bool = True) -> list:
     """Функция сортирует операции по дате"""
     sort_by_date_list = sorted(operations, key=lambda item: item["date"], reverse=flow)
     return sort_by_date_list
-
